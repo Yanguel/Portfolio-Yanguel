3a010c6d625ac8963a59b5778b3c642e46502091
yanguel <yanguelcornillet@yahoo.com>
2022-12-15 16:59:17 +0100

    Mise en place du dossier , suppression des fichiers inutiles

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './styles/accueil.css'
+import Accueil from './pages/accueil'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Accueil />
+  </React.StrictMode>
+)
